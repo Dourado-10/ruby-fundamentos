a128c00d99fb881051fbcab81f0d61ffb3b83b40
Gilson-Dourado10 <gilsondourado10@gmail.com>
2022-02-28 22:06:48 -0300

    usando o for

---
+++ fundamentos.rb
@@ -0,0 +1,19 @@
+
+
+
+fruits = ['Maçã', 'Uva', 'Morango']
+
+for fruit in fruits
+ puts fruit
+end
+puts "-------------------------------------------"
+
+fruits = ['Maçã', 'Uva', 'Morango']
+
+fruit = "Laranja"
+
+for fruit in fruits
+  puts fruit
+end
+
+puts fruit
---
+++ study.rb
@@ -0,0 +1,9 @@
+  puts "Por favor digite seu nome"
+  name = gets.chomp
+  puts " #{name}começando a aprender o Ruby hoje !"
+  puts " levantando ás 3:25 da manhã pra estudar "
+  puts " começando com a base wque é lógica para o resto ficar mais fácil!!!! "
+  puts "VAMOS PARA CIMA PQ TEM COISA BOA NA FRENTE PRA NÓS"
+  puts  "2co cap 9.6 aquele que semeia pouco, pouco também colherá e aquele que semeia com abundância, com fartura colherá
+  "
+  puts "Esse úm príncípio biblico que eu estou levando comigo.  "
